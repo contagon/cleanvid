a799dea6e64ab88834284232fe21d587b5ddb62b
Easton Potokar <contagon6@gmail.com>
2022-12-28 20:29:04 -0500

    Large changes to monitoring script

--- src/cleanvid/monitor.py
+++ src/cleanvid/monitor.py
@@ -1,6 +1,8 @@
+from ftplib import all_errors
 import os
 import json
-import paho.mqtt.client as paho
+import paho.mqtt.subscribe as subscribe
+import paho.mqtt.publish as publish
 import time
 from glob import iglob
 from cleanvid import VidCleaner, GetSubtitles
@@ -19,97 +21,85 @@ TOPIC_ERROR = os.path.join(MQTT_TOPIC, "error")
 DIR = os.environ.get("DIRECTORY", "/data")
 WAIT = float(os.environ.get("WAIT", 60))
 
-def connect_mqtt_client():
-    """CONNECT TO MQTT Broker """
-    client = paho.Client()
-    client.username_pw_set(MQTT_USER, password=MQTT_PASS)
-    client.connect(MQTT_ADDRESS, MQTT_PORT)
+def read_lines(file):
+    with open(os.path.join(DIR, file), "r+") as f:
+        lines = {line.rstrip('\n') for line in f}
+    return lines
 
-    return client
+def write_lines(file, lines):
+    with open(os.path.join(DIR, file), "w+") as f:
+        for s in sorted(lines):
+            f.write(s+"\n")
 
-def wait_msg(client, topic):
-    """Get message from MQTT"""
-    # Listen for message
-    global data
-    data = []
-    def on_message(client, userdata, msg):
-        global data
-        data = json.loads(msg.payload)
-
-    # subscribe to topic
-    client.on_message = on_message
-    client.subscribe(topic)
-
-    # Wait till we have a response
-    while len(data) == 0:
-        client.loop()
-        time.sleep(0.1)
-
-    client.unsubscribe(topic)
-
-    return data
-
-def check_new_shows(client):
+def check_new_shows():
     """Check for any new folders"""
     # Read in all shows that we've already noticed
-    with open(os.path.join(DIR, "found.txt")) as f:
-        found_shows = {line.rstrip('\n') for line in f}
+    found_shows = read_lines("found.txt")
 
     # Read in all shows with folders
     all_shows = {f.name for f in os.scandir(DIR) if f.is_dir()}
-
-    # Save those to file
-    if len(all_shows) > 0:
-        with open(os.path.join(DIR, "found.txt"), "w") as f:
-            for s in sorted(all_shows):
-                f.write(s+"\n")
 
     # Publish them
     new_shows = all_shows - found_shows
     if len(new_shows) > 0:
         logging.info(f"Found new folders: {new_shows}")
-        client.publish(TOPIC_FOUND, json.dumps(list(new_shows)), retain=True)
-        time.sleep(1)
+        publish.single(
+            TOPIC_FOUND, 
+            payload=json.dumps(list(new_shows)),
+            qos=1,
+            hostname=MQTT_ADDRESS, 
+            port=MQTT_PORT,
+            auth={"username": MQTT_USER, "password": MQTT_PASS}
+        )
+
+        # Save those to file
+    if len(new_shows) > 0:
+        write_lines("found.txt", all_shows)
 
     return new_shows
 
-def check_new_clean(client, are_new_shows):
+def check_new_clean(are_new_shows):
     """Gets new shows if there is any, otherwise reads in cleaning list"""
     # Get shows in file, and any messages
-    with open(os.path.join(DIR, "cleaning.txt")) as f:
-        to_clean = {line.rstrip('\n') for line in f}
+    to_clean = read_lines("cleaning.txt")
 
     if are_new_shows:
         logging.info("Waiting to hear if should monitor new shows...")
-        new_to_clean = set(wait_msg(client, TOPIC_ADD))
+        new_to_clean = subscribe.simple(
+            TOPIC_ADD,
+            hostname=MQTT_ADDRESS, 
+            port=MQTT_PORT,
+            auth={"username": MQTT_USER, "password": MQTT_PASS}
+        )
+        new_to_clean = set(json.loads(new_to_clean.payload))
         logging.info(f"Now monitoring: {new_to_clean}")
         to_clean |= new_to_clean
 
-    # Rewrite all shows to file
-    with open(os.path.join(DIR, "cleaning.txt"), "w") as f:
-        for s in sorted(to_clean):
-            f.write(s+"\n")
+        # Rewrite all shows to file
+        write_lines("cleaning.txt", to_clean)
 
     return to_clean
 
-def clean(client, to_clean):
+def clean(to_clean):
     """Clean all folders found in the cleaning file"""
-    errors = []
-    logging.info("Beginning cleaning")
+    errors = set()
+    logging.info("Beginning all cleaning")
     for s in to_clean:
         # Find all files that need to be cleaned
         dir = os.path.join(DIR, s, "**/*")
-        files = [f for f in iglob(dir, recursive=True) if os.path.isfile(f) and "_clean" not in f and os.path.splitext(f)[1] != ".srt"]
+        files = [f for f in iglob(dir, recursive=True) if os.path.isfile(f) 
+                                                        and "_clean" not in f 
+                                                        and os.path.splitext(f)[1] != ".srt"]
         files = sorted(files)
 
         if len(files) > 0:
-            logging.info(f"Cleaning {s}...")
+            logging.info(f"[{s}] Beginning Cleaning")
 
         # Iterate through them
         for f in files:
             try:
                 file_name = os.path.basename(f)
-                logging.info(f"Starting {file_name}")
+                logging.info(f"[{file_name}] Subtitles")
 
                 # get subtitle and output files
                 subs = GetSubtitles(f, 'eng', False)
@@ -127,23 +117,36 @@ def clean(client, to_clean):
                     fullSubs=True,
                 )
                 cleaner.CreateCleanSubAndMuteList()
+                logging.info(f"[{file_name}] Cleaning Video")
                 cleaner.MultiplexCleanVideo()
 
                 # Remove the old files, and extra subtitles
-                logging.info(f"Removing {file_name} and it's files...")
+                logging.info(f"[{file_name}] Removing extras")
                 os.remove(f)
                 os.remove(subs)
 
             except:
-                logging.warning(f"{file_name} failed")
-                errors.append(file_name)
+                logging.warning(f"[{file_name}] Failed")
+                errors.add(file_name)
 
     logging.info("Finished cleaning")
     if len(errors) > 0:
-        logging.warning(f"Had errors on files: {errors}")
-        # Send error messages for HA to follow up on
-        client.publish(TOPIC_ERROR, json.dumps(list(errors)), retain=True)
-        time.sleep(1)
+        old_errors = read_lines("errors.txt")
+        new_errors = errors - old_errors
+
+        if len(new_errors) > 0:
+            logging.warning(f"Had errors on files: {new_errors}")
+            # Send error messages for HA to follow up on
+            publish.single(
+                TOPIC_ERROR, 
+                payload=json.dumps(list(new_errors)),
+                qos=1,
+                hostname=MQTT_ADDRESS, 
+                port=MQTT_PORT,
+                auth={"username": MQTT_USER, "password": MQTT_PASS}
+            )
+            write_lines("errors.txt", errors|old_errors)
+        
 
 def Monitor():
     # Setup logging
@@ -152,17 +155,26 @@ def Monitor():
          level=logging.INFO,
          datefmt='%Y-%m-%d %H:%M:%S')
 
+    # Turn off a bunch of logging
+    logging.getLogger('subliminal.providers.opensubtitles').setLevel(logging.WARNING)
+    logging.getLogger('subliminal.providers.podnapisi').setLevel(logging.WARNING)
+    logging.getLogger('subliminal.score').setLevel(logging.WARNING)
+    logging.getLogger('subliminal.subtitle').setLevel(logging.WARNING)
+    logging.getLogger('subliminal.video').setLevel(logging.WARNING)
+    logging.getLogger('subliminal').setLevel(logging.WARNING)
+
+    # for key in logging.root.manager.loggerDict:
+    #     print(key)
+    # return
+
     # Get started
-    logging.info("Connecting to MQTT...")
-    client = connect_mqtt_client()
-    logging.info("Connected")
     while True:
         # Check for any new folders
-        new_shows = check_new_shows(client)        
+        new_shows = check_new_shows()        
         # check if there's new shows to be cleaned
-        to_clean = check_new_clean(client, len(new_shows) > 0)
+        to_clean = check_new_clean(len(new_shows) > 0)
         # Do the cleaning!
-        clean(client, to_clean)
+        clean(to_clean)
         # wait
         time.sleep(WAIT*60)
 
